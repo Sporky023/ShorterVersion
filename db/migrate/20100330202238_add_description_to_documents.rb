ecd431d00c1d1b8d25517503c877a7fcd1a69ab1
Luke Griffiths <lookmeup>
2010-03-30 14:46:11 -0600

    added missing migration file

---
+++ db/migrate/20100330202238_add_description_to_documents.rb
@@ -0,0 +1,13 @@
+class AddDescriptionToDocuments < ActiveRecord::Migration
+  def self.up
+    
+    add_column :documents, :description, :text
+  
+  end
+
+  def self.down
+    
+    remove_column :documents, :description
+    
+  end
+end
