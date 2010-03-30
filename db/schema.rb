01d727ef8f47c4e031c817531d0940db01b9a4a7
Luke Griffiths <lookmeup>
2010-03-30 14:44:00 -0600

    added document descrptions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100330194710) do
+ActiveRecord::Schema.define(:version => 20100330202238) do
 
   create_table "documents", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20100330194710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "number_of_pages"
+    t.text     "description"
   end
 
   create_table "summaries", :force => true do |t|
