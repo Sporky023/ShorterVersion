c3032c419d453b7432f92cc0a69a84bb0dde59ec
Luke Griffiths <lookmeup>
2010-04-09 15:55:56 -0700

    made skinny page option available when writing summaries

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 	$('.show_body').click(function(){
 		//alert("this = "+$(this))
 		//alert("this.parents(:first) = "+$(this).parents(':first'))
-		alert($(this).parents('.module').children('.body:first').show())//.show()
 		$(this).parents('.module:first').children('.body:first').show()
 	})
 	
 })
+
+skinny_window = function(){
+	$('.help').hide()
+	window.resizeTo(340, 600)
+}
