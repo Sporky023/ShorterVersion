430534cd8f24b8ee0f840a1020f1b119dc5aa30e
Luke <luke@luke-laptop.(none)>
2010-06-05 16:12:13 -0400

    now showing most recent documents first on homepage

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,7 +5,7 @@ class DocumentsController < ApplicationController
   # GET /documents
   # GET /documents.xml
   def index
-    @documents = Document.all
+    @documents = Document.all(:order => "created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
