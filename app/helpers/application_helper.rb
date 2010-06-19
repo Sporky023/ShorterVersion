7845dc66f14bcb8ceba8c2ca006bdbf422077863
Luke Griffiths <luke@lukegriffiths.net>
2010-06-19 19:55:37 -0400

    now have devise running and other stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,8 @@ module ApplicationHelper
 
 
   def facebook_share_button
-    '<a name="fb_share" type="button_count" href="http://www.facebook.com/sharer.php">Share</a><script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>'
+    #'<a name="fb_share" type="button_count" href="http://www.facebook.com/sharer.php">Share</a><script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>'
+    '<a name="fb_share" type="icon_link" href="http://www.facebook.com/sharer.php">Share</a><script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>'
   end
   
   def options_for_summary_type_of_subset
